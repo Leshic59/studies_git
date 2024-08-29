a6d6328039b37042e193abb0d692ea45ff7f9da4
Aleksei Yurtaikin <Leshic59@gmail.com>
2024-08-29 23:45:16 +0300

    added getMessage() method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     private static String msg = "Hello qa.quru!!!!!";
 
     public static void main(String [] args) {
-        System.out.println("getMessage()");
+        System.out.println(getMessage());
     }
 
     private static String getMessage() {
