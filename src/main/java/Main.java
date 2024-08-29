9453c618b0d97c74186209ebaba6ad021f846d3f
Aleksei Yurtaikin <Leshic59@gmail.com>
2024-08-29 23:41:00 +0300

    added getMessage() method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import org.codehaus.groovy.runtime.StringGroovyMethods;
+
 public class Main {
     public static void main(String [] args) {
-        System.out.println("Hello qa.quru!");
+        System.out.println("getMessage()");
+    }
+
+    private static StringGroovyMethods getMessage() {
+        return "Hello qa.quru!";
     }
 }
