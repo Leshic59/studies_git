3d5ecfc7c922d6560783199aa654e43e091045b5
Aleksei Yurtaikin <Leshic59@gmail.com>
2024-08-29 23:40:11 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
+
+    private static String msg = "Hello qa.quru!!!!!";
+    
     public static void main(String [] args) {
-        System.out.println("Hello qa.quru!!!!!");
+        System.out.println(msg);
     }
 }
